460f512b39daed5c9729d004207f2c3b9f25a7d7
yamasy1549 <sanae@yamasy.info>
2016-05-08 00:08:52 +0900

    数式のfont-sizeを調整

--- src/scripts/mathjax.js
+++ src/scripts/mathjax.js
@@ -5,6 +5,8 @@ $("td").each(function() {
 });
 
 setTimeout(function() {
-    $(".jx-chtml, .MathJax_CHTML").css("margin", "0 0.2em");
+    $(".jx-chtml, .MathJax_CHTML")
+        .css("margin", "0 0.2em")
+        .css("font-size", "100%");
     // MathjaxがCSSを上書きするので、それが終わってから
 }, 2000);
