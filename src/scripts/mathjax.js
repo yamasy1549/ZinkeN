4e26c9e69b114180bbc1b3aba640b82cefac6234
yamasy1549 <sanae@yamasy.info>
2016-05-07 21:16:15 +0900

    表中の数字を自動的に右寄せ

--- src/scripts/mathjax.js
+++ src/scripts/mathjax.js
@@ -1,3 +1,9 @@
+$("td").each(function() {
+    if(this.innerHTML.match(/\d/g) != null) {
+        this.style.textAlign = "right";
+    }
+});
+
 setTimeout(function() {
     $(".jx-chtml, .MathJax_CHTML").css("margin", "0 0.2em");
     // MathjaxがCSSを上書きするので、それが終わってから
