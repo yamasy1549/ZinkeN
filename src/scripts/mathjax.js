7f99b823e7c35b2b2cad6e491a52043400413d98
yamasy1549 <sanae@yamasy.info>
2016-05-14 00:29:39 +0900

    スタイル微調整

--- src/scripts/mathjax.js
+++ src/scripts/mathjax.js
@@ -4,13 +4,11 @@ $("td").each(function() {
     }
 });
 
-setTimeout(function() {
+(function() {
     $(".jx-chtml, .MathJax_CHTML")
-        .css("margin", "0 0.2em")
-        .css("font-size", "100%");
+        .css("font-size", "110%");
     $(".MJXc-display")
         .css("font-size", "120%");
     $(".mjx-sub .mjx-char")
         .css("transform", "scale(0.8)");
-    // MathjaxがCSSを上書きするので、それが終わってから
-}, 2000);
+});
